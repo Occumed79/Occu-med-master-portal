4fbf222a030ff7724fd84d2f31de882301dbb01e
Occumed79 <Alex.Ayvazian@logan.edu>
2026-06-02 16:34:40 -0700

    Remove login routes from portal app

--- occu-med-portal/src/App.tsx
+++ occu-med-portal/src/App.tsx
@@ -6,8 +6,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import Admin from "@/pages/Admin";
-import Login from "@/pages/Login";
-import SetupAccount from "@/pages/SetupAccount";
 import { loadPortalState } from '@/lib/portalBackend';
 
 const queryClient = new QueryClient();
@@ -72,8 +70,6 @@ function Router() {
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/admin" component={Admin} />
-      <Route path="/login" component={Login} />
-      <Route path="/setup-account" component={SetupAccount} />
       <Route component={NotFound} />
     </Switch>
   );
